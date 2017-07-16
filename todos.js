55a2f4446e0611204618292779223f6046136d5c
John Furtado <jf8846@gmail.com>
2017-07-16 20:15:19 +0000

    intial commit

--- todos.js
+++ todos.js
@@ -1,24 +1,4 @@
-// creat list(array) of TODOS
-var todos = ["item1", "item2", "item3"];
+var todoList = {
+    todos: ["item1", "item2", "item3"]
+};
 
-function displayTODOS() {
-    console.log("My TODOS:", todos);
-}
-
-function addTODOS(todo) {
-    displayTODOS();
-    todos.push(todo);
-    displayTODOS();
-}
-
-function changeTODOS(position, newValue) {
-    displayTODOS();
-    todos[position] = newValue;
-    displayTODOS();
-}
-
-function deleteTODOS(position) {
-    displayTODOS();
-    todos.splice(position, 1);
-    displayTODOS();
-}
