c2e2d5b86073ed15ab92b79619635fcdcd5ffadf
John Furtado <jf8846@gmail.com>
2017-07-16 20:33:03 +0000

    display and add todos in an obect

--- todos.js
+++ todos.js
@@ -1,7 +1,13 @@
 var todoList = {
     todos: ["item1", "item2", "item3"],
-    displayTodos: function () {
+    displayTodos: function() {
         console.log('My Todos:', this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
     }
 };
 
+todoList.displayTodos();
+todoList.addTodo('item4');
